05c01f1798cfd0ecc54e5d91f555c668a0fd0c6d
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2018-05-16 16:31:32 -0500

    Added a terrain prefab

---
+++ Assets/Scripts/SpinIcon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinIcon : MonoBehaviour {
+
+    float speed = 100;
+	
+    private void FixedUpdate()
+    {
+        Spin();
+    }
+
+    private void Spin()
+    {
+        transform.Rotate(Vector3.up, speed* 2 * Time.deltaTime);
+         
+    }
+}
