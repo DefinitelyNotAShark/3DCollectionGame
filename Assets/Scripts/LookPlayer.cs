61c863fd344a87e26dd7340f2ceecab610feec59
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2018-05-21 16:53:57 -0500

    Can sort of move around. Look script does not yet work

---
+++ Assets/Scripts/LookPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private float mouseSensitivity = 100;
+
+    [SerializeField]
+    private float clampAngle = 80;
+
+    private float lookX = 0;
+    private float lookY = 0;
+
+    private float mouseX;
+    private float mouseY;
+
+    private Vector3 rotation;
+
+
+    private void Start()
+    {
+        rotation = transform.localRotation.eulerAngles;
+        lookX = rotation.x;
+        lookY = rotation.y;
+    }
+
+    private void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+        lookX += mouseX * mouseSensitivity * Time.deltaTime;
+        lookY += mouseY * mouseSensitivity * Time.deltaTime;
+
+        lookX = Mathf.Clamp(lookX, -clampAngle, clampAngle);
+
+        Quaternion locRotation = Quaternion.Euler(rotation.x, rotation.y, 0);
+
+        transform.rotation = locRotation;        
+    }
+}
+
---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+    private float horizontal;
+    private float vertical;
+
+    [SerializeField]
+    private float speed = 10;
+
+    private Vector3 position;
+    private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update ()
+    {
+        GetInput();
+	}
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void GetInput()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void Move()
+    {
+        position = new Vector3(horizontal * Time.deltaTime * speed, 0, vertical * Time.deltaTime * speed);
+        //rigidbody.AddForce(position);
+        transform.Translate(position);
+    }
+}
