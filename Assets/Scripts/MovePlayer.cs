4a74488b7efee8729e477c494bd3685b8142eaf4
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2018-05-27 17:21:15 -0500

    Movement is more polished. Jump works but could be a bit more crisp

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -1,21 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+    private bool isOnGround;
 
     private string movementAxisName;
     private string turnAxisName;
+    private string jumpAxisName;
 
     private float movementAxisValue;
     private float turnAxisValue;
+    private float jumpAxisValue;
 
     [SerializeField]
     private float speed = 10;
 
     [SerializeField]
     private float turnSpeed = 100;
+
+    [SerializeField]
+    private float jumpHeight = 1;
 
     private Vector3 position;
     private Rigidbody rigidbody;
@@ -24,12 +31,19 @@ public class MovePlayer : MonoBehaviour
     {
         movementAxisName = "Vertical";
         turnAxisName = "Horizontal";
+        jumpAxisName = "Jump";
         rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         GetInput();
+
+        if (jumpAxisValue > 0 && isOnGround)
+        {
+            rigidbody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            isOnGround = false;
+        }
     }
 
     private void FixedUpdate()
@@ -42,10 +56,11 @@ public class MovePlayer : MonoBehaviour
     {
         movementAxisValue = Input.GetAxis(movementAxisName);//stores my input for each update
         turnAxisValue = Input.GetAxis(turnAxisName);
+        jumpAxisValue = Input.GetAxis(jumpAxisName);
     }
 
     void Move()
-    {//DEBUG check if transform.forward actually works
+    { 
         Vector3 movement = transform.forward * movementAxisValue * speed * Time.deltaTime;//sets a vector3 to forward times input value
         rigidbody.MovePosition(rigidbody.position + movement);//moves my rigidbody the vector3
     }
@@ -55,6 +70,10 @@ public class MovePlayer : MonoBehaviour
         float turn = turnAxisValue * turnSpeed * Time.deltaTime;
         Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);//this creates a q for the turning
         rigidbody.MoveRotation(rigidbody.rotation * turnRotation);//this actually turns it
+    }
 
+    private void OnCollisionStay(Collision collision)
+    {
+        isOnGround = true;
     }
 }
