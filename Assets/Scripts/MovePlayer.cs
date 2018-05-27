827e8cb7da6ff8344bc9afc6d6bf605e973956ca
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2018-05-27 16:15:42 -0500

    player is a cube. Needs lots of moving polish and direction is wrong

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -2,42 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovePlayer : MonoBehaviour {
+public class MovePlayer : MonoBehaviour
+{
 
-    private float horizontal;
-    private float vertical;
+    private string movementAxisName;
+    private string turnAxisName;
+
+    private float movementAxisValue;
+    private float turnAxisValue;
 
     [SerializeField]
     private float speed = 10;
+
+    [SerializeField]
+    private float turnSpeed = 100;
 
     private Vector3 position;
     private Rigidbody rigidbody;
 
     private void Start()
     {
+        movementAxisName = "Vertical";
+        turnAxisName = "Horizontal";
         rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update ()
+    void Update()
     {
         GetInput();
-	}
+    }
 
     private void FixedUpdate()
     {
         Move();
+        Turn();
     }
 
     void GetInput()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        movementAxisValue = Input.GetAxis(movementAxisName);//stores my input for each update
+        turnAxisValue = Input.GetAxis(turnAxisName);
     }
 
     void Move()
+    {//DEBUG check if transform.forward actually works
+        Vector3 movement = transform.forward * movementAxisValue * speed * Time.deltaTime;//sets a vector3 to forward times input value
+        rigidbody.MovePosition(rigidbody.position + movement);//moves my rigidbody the vector3
+    }
+
+    void Turn()
     {
-        position = new Vector3(horizontal * Time.deltaTime * speed, 0, vertical * Time.deltaTime * speed);
-        //rigidbody.AddForce(position);
-        transform.Translate(position);
+        float turn = turnAxisValue * turnSpeed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);//this creates a q for the turning
+        rigidbody.MoveRotation(rigidbody.rotation * turnRotation);//this actually turns it
+
     }
 }
