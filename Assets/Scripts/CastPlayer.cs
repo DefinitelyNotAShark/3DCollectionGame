ec73cf4cf4646ef0e1af42339584cc8686aecd86
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2018-05-28 02:12:51 -0500

    Trees change color when player can interact. Interface set up to do object highlighting and interaction.

---
+++ Assets/Scripts/CastPlayer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastPlayer : MonoBehaviour
+{// this is my raycast. It's gonna detect items that the player is facing to help interact.
+    [SerializeField]
+    private float distance = 2;
+
+    private Transform cubeCollisionTransform;
+    private Iinteractable interactableInstance;
+    private Vector3 center;
+    private BoxCollider collider;
+    RaycastHit hit;
+    Ray playerSight;
+
+    private void Start()
+    {
+        collider = GetComponent<BoxCollider>();
+    }
+
+    private void Update()
+    {
+        center = collider.bounds.center;
+        UpdatePlayerSight();
+        CheckForCollide();
+    }
+
+    private void UpdatePlayerSight()
+    {
+        playerSight = new Ray(this.transform.position, this.transform.forward);//might have to do vector3.forward
+    }
+
+    private void CheckForCollide()
+    {
+        if(Physics.Raycast(playerSight, out hit, distance))//this occurs if the ray hits something...
+        {
+            interactableInstance = hit.collider.gameObject.transform.parent.GetComponent<Iinteractable>();
+
+            if (interactableInstance != null)
+            {
+                interactableInstance.HighlightObject();
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/DoTreeStuff.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoTreeStuff : MonoBehaviour, Iinteractable
+{
+    Color startColor;
+
+	void Update ()
+    {
+	}
+
+    public void HighlightObject()//only public because has to be for interface
+    {
+        foreach (Transform child in transform)//get the objects in the tree
+        {
+            if (child.GetComponent<MeshRenderer>() != null)//if there is a renderer...
+            {
+                startColor = child.GetComponent<MeshRenderer>().material.color;//get the color it started with
+                child.GetComponent<MeshRenderer>().material.color = Color.blue;//change the color to the material color
+            }
+        }
+    }
+
+    public void UnHighlightObject()
+    {
+        foreach (Transform child in transform)//get the objects in the tree
+        {
+            if (child.GetComponent<MeshRenderer>() != null)//if there is a renderer...
+            {
+                child.GetComponent<MeshRenderer>().material.color = startColor;//change the color to the material color
+            }
+        }
+    }
+
+    public void Interact()
+    {
+
+    }
+}
