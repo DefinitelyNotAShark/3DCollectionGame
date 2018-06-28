6b4314ab8d94dc61e00cf7a3e4ccaba9b154c92e
DefinitelyNotAShark <32074072+DefinitelyNotAShark@users.noreply.github.com>
2018-06-27 19:20:54 -0500

    Npc being set up. Need to program random movements and text box with maybe bunny avatar.

---
+++ Assets/Scripts/NPCScripts/SetNPCColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetNPCColor : MonoBehaviour {
+
+    [SerializeField]
+    private Color npcColor;
+
+    private MeshRenderer[] renderers;
+
+	// Use this for initialization
+	void Start ()
+    {
+        renderers = GetComponentsInChildren<MeshRenderer>();//put all renderers in array
+        for(int i = 0; i < renderers.Length; i++)//for each renderer
+        {
+            renderers[i].material.color = npcColor;//set it to npccolor
+        }
+	}
+}
