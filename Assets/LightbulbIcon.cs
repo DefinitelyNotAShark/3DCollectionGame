43a4e46ec138f9c3a78aa39986ea9bb9672bd982
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-12-03 17:55:36 -0600

    Got a tree to chop

---
+++ Assets/LightbulbIcon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightbulbIcon : MonoBehaviour
+{
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponentInChildren<Image>();
+    }
+
+    public void SetIcon(bool iconState)
+    {
+        if (iconState == true)
+            image.enabled = true;
+        else if (iconState == false)
+            image.enabled = false;
+    }
+
+}
